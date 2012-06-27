6fc267e4b9b4014ab0af284a26a91332fe5e9da0
sumit-modi <sumit.modi.47@gmail.com>
2012-06-27 13:55:05 +0530

    Update master

--- TestFizzBuzzStubbed.py
+++ TestFizzBuzzStubbed.py
@@ -6,18 +6,39 @@ Q3. What will be printed when we execute 'python FizzBuzzStubbed.py' ? [3 pts]
 
 
 
-
+Ans:_
+setUpClass FizzBuzzStubbed
+setup
+test_report
+teardown
+.setup
+test_report
+teardown
+.tearDownClass
 
 
 
 
 
 Q4. Implement MyStub class so that you can send it as a fake object to the
-    report method of FizzBuzz object from a test case. [3 pts]
+report method of FizzBuzz object from a test case. [3 pts]
 
 """
 class MyStub(object):
-    pass
+    def __init__(self):
+        self.openflag=False
+        self.values=[] # create empty list
+
+    def open(self):
+        self.openflag = True #"open fake file"
+
+    def write(self,msg):
+        self.values.append(msg) # append values to list
+
+    def close():
+        self.openflag = False # close fake "file"
+
+
 
 
 
